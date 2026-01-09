65d146f7a3d95218f6288c876a2bab48d318e4c6
OgaTS <pachecojorge11@gmail.com>
2026-01-09 00:17:45 -0300

    Nuevo listar tarea

---
+++ to-do-list/src/App.jsx
@@ -0,0 +1,17 @@
+
+import ListarTarea from "./componentes/ListarTarea"
+
+
+function App() {
+
+  return (
+    <>
+      <h1>To do List</h1>
+
+      <ListarTarea/>
+
+    </>
+  )
+}
+
+export default App
---
+++ to-do-list/src/componentes/TareaItem.jsx
@@ -0,0 +1,27 @@
+
+
+const TareaItem = ({tarea, onComplete, onEliminar}) =>{
+
+    return(
+        <div style={{ 
+            border: '1px solid #ccc', 
+            margin: '10px', 
+            padding: '10px',
+            display: 'flex',
+            justifyContent: 'space-between',
+            backgroundColor: tarea.complete ? '#f9f9f9' : '#fff'
+            }} >
+            <h3 style={{ textDecoration: tarea.complete ? 'line-through' : 'none' }}>
+                {tarea.text}
+            </h3>
+
+            <div>
+                <input type="checkbox" checked={tarea.complete} onChange={() => onComplete(tarea.id)} />
+                <button onClick={() =>onEliminar(tarea.id)}>Eliminar Tarea</button>
+            </div>
+        
+        </div>
+    )
+}
+
+export default TareaItem
