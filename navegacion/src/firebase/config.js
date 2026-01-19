b3871c16b9b83c4675c1225c5dafeb3a966ebfc4
OgaTS <pachecojorge11@gmail.com>
2026-01-19 17:43:28 -0300

    Actualizacion del config.js

--- navegacion/src/firebase/config.js
+++ navegacion/src/firebase/config.js
@@ -18,25 +18,3 @@ const app = initializeApp(firebaseConfig);
 // Exportamos las herramientas para usarlas en otros archivos
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
-
-// IMPORTANTE JORGE DEL FUTURO ESTOS DATOS SON IMPORTANTES PARA VER LOS DATOS DE LA PAGINA
-
-
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCq0KWzs3FdT0Q222qCUWohTwNkPmYTr9A",
-//   authDomain: "mi-super-app-3a0f5.firebaseapp.com",
-//   projectId: "mi-super-app-3a0f5",
-//   storageBucket: "mi-super-app-3a0f5.firebasestorage.app",
-//   messagingSenderId: "691237583392",
-//   appId: "1:691237583392:web:f0313c6f0096be84831a45"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
